6ced04d288cdfd7a3a4cbfbea94304c33a9d25c1
Vandesm14 <vandesm14@gmail.com>
2020-03-13 22:19:36 -0400

    format code

---
+++ script.js
@@ -0,0 +1,204 @@
+var cards = [];
+var backgrounds = [];
+var colors = ['red', 'orange', 'green', 'cyan', 'blue', 'purple'];
+
+var theme = true;
+var viewMode = false;
+var slide = 0;
+
+var slideBack = ['ArrowDown', 'ArrowLeft', 'Backspace'];
+var slideForward = ['ArrowUp', 'ArrowRight', ' ', 'Enter'];
+
+var vw = $(window).width();
+var clipboardPerms = false;
+
+$(document).ready(function () {
+	updateCards();
+
+	$(document).on('keydown', function (e) {
+		if (e.key === 'Escape') {
+			viewMode = !viewMode;
+			view();
+		} else if (e.key === 'Tab' && viewMode) {
+			e.preventDefault();
+			theme = !theme;
+			updateFrames();
+		} else if (e.key === 'F2' && !viewMode) {
+			if (!clipboardPerms) {
+				testPermission();
+			}
+			navigator.clipboard.readText()
+				.then(text => {
+					cards = text.split('\n').filter(el => el);
+					fromCards();
+				})
+				.catch(err => {
+					alert('Error pasting item');
+				});
+		} else if (e.key === 'F3' && !viewMode) {
+			e.preventDefault();
+			if (!clipboardPerms) {
+				testPermission();
+			}
+			navigator.clipboard.writeText(cards.join('\n'))
+				.then(() => {
+					alert('Item Copied!');
+				})
+				.catch(err => {
+					alert('Error copying item<br>' + err);
+				});
+		}
+	});
+
+	$(document).on('keydown', function (e) {
+		let step = false;
+		if (viewMode) {
+			if (slideBack.includes(e.key)) {
+				slide = slide - 1 < 0 ? 0 : slide - 1;
+			} else if (slideForward.includes(e.key)) {
+				slide = slide + 1 >= cards.length ? cards.length - 1 : slide + 1;
+			} else if (e.key === 'Home') {
+				step = true;
+				slide = 0;
+			} else if (e.key === 'End') {
+				step = true;
+				slide = cards.length - 1;
+			}
+			setView(step);
+		}
+	});
+
+	$(document).on('click', function () {
+		if (viewMode) {
+			slide = slide + 1 >= cards.length ? cards.length - 1 : slide + 1;
+			setView();
+		}
+	});
+
+	$(document).on('contextmenu', function (e) {
+		if (viewMode) {
+			e.preventDefault();
+			slide = slide - 1 < 0 ? 0 : slide - 1;
+			setView();
+		}
+	});
+
+	$(window).on('resize', function () {
+		vw = $(window).width();
+		setView();
+	});
+});
+
+function updateCards() {
+	if ($('#list').children().length === 0) {
+		let template = $('#cardTemplate').html();
+		$('#list').append(template);
+		$('#viewer').html('<p class="frame" style="background-color:white;color:black">No slides</p>');
+	}
+	$('#list > .card').each(function () {
+		$(this).find('.card-id').text($(this).index() + 1);
+	});
+	calcListeners();
+	calcCards();
+	createSortable($('#list')[0]);
+}
+
+function calcListeners() {
+	$('.button-add').off('click');
+	$('.button-add').on('click', function () {
+		let template = $('#cardTemplate').html();
+		$(this).closest('.card').after(template);
+		updateCards();
+	});
+
+	$('.button-remove').off('click');
+	$('.button-remove').on('click', function () {
+		$(this).closest('.card').remove();
+		updateCards();
+	});
+
+	$('.text').off('keyup');
+	$('.text').on('keyup', function () {
+		$(this).css('height', '5px');
+		$(this).css('height', $(this)[0].scrollHeight + 'px');
+		calcCards();
+		updateFrames();
+	});
+}
+
+function calcCards() {
+	cards = [];
+	$('.card').each(function (el) {
+		cards.push($(this).find('.text').val());
+	});
+	updateFrames();
+}
+
+function fromCards() {
+	$('#list').empty();
+	for (let card of cards) {
+		let template = $('#cardTemplate').html();
+		$('#list').append(template);
+		$('.card:last-child').find('.text').val(card);
+	}
+	updateCards();
+	updateFrames();
+}
+
+function view() {
+	if (viewMode) {
+		$('#editor').hide();
+		$('#viewer').show();
+	} else {
+		$('#editor').show();
+		$('#viewer').hide();
+	}
+}
+
+function setView(step = false) {
+	if (step) {
+		$('#viewer').css('margin-left', -vw * slide);
+	} else if (theme) {
+		$('#viewer').stop(true, true);
+		$('#viewer').animate({
+			marginLeft: -vw * slide
+		}, 300);
+	} else if (!theme) {
+		$('#viewer').stop(true, true);
+		$('#viewer').css('margin-left', -vw * slide);
+	}
+	$('.show').removeClass('show');
+	$('.viewer > .frame').eq(slide).addClass('show');
+}
+
+function updateFrames() {
+	$('#viewer').empty();
+	for (let i in cards) {
+		$('#viewer').append(`<p class="frame ${colors[i % colors.length]}">${cards[i]}</p>`);
+	}
+	if (!theme) {
+		for (let color of colors) {
+			$(`.frame.${color}`).removeClass(color);
+		}
+	}
+}
+
+function createSortable(el) {
+	Sortable.create(el, {
+		animation: 150,
+		invertSwap: true,
+		onEnd: calcCards
+	});
+}
+
+function testPermission() {
+	navigator.clipboard.readText()
+		.then(text => {
+			clipboardPerms = true;
+			console.log('HasPerms');
+		})
+		.catch(() => {
+			console.log('NoPerms');
+			alert('Please allow clipboard permissions');
+		});
+}
