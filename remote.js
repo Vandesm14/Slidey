68a1442742ba1341ecc41feb525c5059c2a6d2d6
Vandesm14 <vandesm14@gmail.com>
2020-06-12 08:48:40 -0400

    fix remote system

--- remote.js
+++ remote.js
@@ -2,9 +2,10 @@ const socket = io.connect('https://Socketio--vandesm14.repl.co');
 
 var id = location.href.match(/\?id=[\w\d]{8}-[\w\d]{4}-[\w\d]{4}-[\w\d]{4}-[\w\d]{12}/);
 const storedid = localStorage.getItem('remoteid');
-if (id && id[0].substr(4) !== storedid) {
+if (id) {
 	id = id[0].substr(4);
 	localStorage.setItem('remoteid', id);
+	window.history.replaceState({}, document.title, window.location.href.replace(window.location.search, ''));
 } else if (storedid) {
 	id = storedid;
 }
